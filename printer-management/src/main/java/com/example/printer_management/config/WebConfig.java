465e90586e8756cad7a5c536c2553fcfcc2703e2
Akachi Anabanti <125439280+Akachi-Anabanti@users.noreply.github.com>
2024-09-26 14:21:49 +0000

    allowed origins update

--- printer-management/src/main/java/com/example/printer_management/config/WebConfig.java
+++ printer-management/src/main/java/com/example/printer_management/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://effective-acorn-pv44jrwv9jph6vjx-3000.app.github.dev/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
