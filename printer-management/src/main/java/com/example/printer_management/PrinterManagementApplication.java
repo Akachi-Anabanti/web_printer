b32d9672c52b79ed5700c485b6fcd17b6205ad30
Akachi Anabanti <125439280+Akachi-Anabanti@users.noreply.github.com>
2024-09-26 13:53:41 +0000

    spring backend with cors and maven

---
+++ printer-management/src/main/java/com/example/printer_management/PrinterManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.printer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class PrinterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterManagementApplication.class, args);
+	}
+
+}
